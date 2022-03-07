ff2c19214dbf998cf31f00ed5db6fae491efb4bf
Marcelo <mleandrofc@yahoo.com>
2022-03-07 19:16:40 -0300

    front-end Header

---
+++ git.js
@@ -0,0 +1,11 @@
+import react from 'react-native-screens';
+import {Text} from 'react-native-gesture-handler';
+
+
+const Hello = () => {
+    return (
+        <Text>Opaaa</Text>
+    )
+}
+
+export default Hello;
